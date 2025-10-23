14d86dd9dc8fbbe76e000901d783a060f4a1c0e9
isharaMadhushankha <isharamadushanka520@gmail.com>
2025-10-24 01:04:17 +0530

    started to create the frontend🧩

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ProductCard from './Components/ProductCard.jsx'
+
+function App() {
+  
+  return (
+    <>
+    <ProductCard name="bluestone" dis="This is a description of the product." price="$500" img="https://us.123rf.com/450wm/bemphoto/bemphoto2303/bemphoto230301011/200261985-red-ruby-stone-texture-background-ai-generated-image.jpg"/>
+    <ProductCard name="redtone" dis="This is a description of the product." price="$600" img="https://us.123rf.com/450wm/bemphoto/bemphoto2303/bemphoto230301011/200261985-red-ruby-stone-texture-background-ai-generated-image.jpg"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ProductCard.css";
+
+const ProductCard = (props) => {
+    console.log(props);
+  return (
+    <div className="prodductCard">
+        <img className="productimage" src={props.img}/>
+        <h2>{props.name}</h2>
+        <p>{props.dis}</p>
+        <p>{props.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
